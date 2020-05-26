e2afb856726f8065704a8be223ff104dc696e11d
raizen62 <vasile.razvan93@gmail.com>
2020-05-26 11:20:19 +0300

    small fix

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 window.addEventListener('load', () => {
-    navigator.serviceWorker.register('serviceworker.js');
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('serviceworker.js');
+    }
 });
+
+document.querySelector('#show-simple-notification').onclick = async () => {
+    const reg = await navigator.serviceWorker.getRegistration();
+
+    Notification.requestPermission().then(permission => {
+        if (permission !== 'granted') {
+            alert('Hi. In order to use the notifications we need to accept this!');
+        } else {
+            const timestamp = new Date().getTime() + 6 * 1000;
+            reg.showNotification(
+                'Hello Netcentricos',
+                {
+                    tag: timestamp, // a unique ID
+                    body: "You should see this!", // content of the notification
+                    // showTrigger: new TimestampTrigger(timestamp), // set the time for the push notification
+                    // data: {
+                    //     url: window.location.href, // pass the current url to the notification
+                    // },
+                    // badge: './assets/badge.png',
+                    icon: 'ella.png',
+                    actions: [
+                        {
+                            action: 'open',
+                            title: 'Open app'
+                        },
+                        {
+                            action: 'close',
+                            title: 'Close notification',
+                        }
+                    ],
+                }
+            );
+        }
+    });
+};
--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,11 @@
-self.addEventListener('install', () => {
-    skipWaiting();
+self.addEventListener('install', event => console.log('Service worker is installed!'));
+
+self.addEventListener('notificationclick', event => {
+
+    // if (event.action === 'close') {
+    //     event.notification.close();
+    // } else {
+    //     self.clients.openWindow('/');
+    // }
+
 });
